fa31fce6cf96f5a4c4c7a6efd513305eb561e2d5
fernandacastro-lang <fernanda.castro.lazaro@gmail.com>
2026-05-19 18:54:03 -0700

    I modified the models to add the administrator section and wrote the admin.py code

--- Queen_Loans/loans/admin.py
+++ Queen_Loans/loans/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Cliente, Prestamo, Pago, Administrador
+
+admin.site.register(Cliente)
+admin.site.register(Prestamo)
+admin.site.register(Pago)
+admin.site.register(Administrador)
--- Queen_Loans/loans/models.py
+++ Queen_Loans/loans/models.py
@@ -49,3 +49,13 @@ class Pago(models.Model):
 
     def __str__(self):
         return f"Pago {self.id} - Préstamo {self.prestamo.id}"
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Administrador(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.usuario.username
